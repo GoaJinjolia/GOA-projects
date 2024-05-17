61b406bbef32574737746b3d840a9be80bcca18e
GoaJinjolia <giojinjo009@gmail.com>
2024-05-17 19:43:43 +0400

    commit

---
+++ day 3/Lesson2.py
@@ -0,0 +1,9 @@
+print(5+5) #mimateba
+print(10-5) #gamokleba
+print(5*5) #gamravleba
+print(25/5) #gayopa
+print("giorgi"+"jinjolia")# sheerteba
+#print 5+5 #shecdoma/error
+name="giorgi"
+firstname="jinjolia"
+print(name+firstname)
