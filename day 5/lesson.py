07ff8f43ee66efa235253664122ffa515868095c
GoaJinjolia <giojinjo009@gmail.com>
2024-05-27 17:39:25 +0400

    commit

---
+++ day 5/lesson.py
@@ -0,0 +1,344 @@
+from turtle import * 
+bgpic("image.png")
+
+# color("green")
+# begin_fill()
+speed(0)
+left(90)
+# forward(1000)
+# right(90)
+
+# forward(1000)
+# right(90)
+
+
+# forward(2000)
+# right(90)
+
+# forward(1000)
+# right(90)
+
+
+# forward(1000)
+# right(90)
+# end_fill()  
+
+# color("blue")
+# begin_fill()
+# left(90)
+# forward(1000)
+# left(90)
+
+# forward(1000)
+# left(90)
+
+
+
+# forward(2000)
+# left(90)
+
+
+# forward(1000)
+# left(90)
+
+
+# forward(1000)
+# left(90)
+
+
+# end_fill()
+
+color("black")
+begin_fill()
+right(90)
+forward(100)
+right(90)
+forward (300)
+
+right(90)
+
+
+forward(200)
+
+right(90)
+forward(300)
+right(90)
+
+
+
+forward(100)
+
+forward(150)
+
+left(90)
+forward (50)
+
+
+left(90)
+forward (50)
+
+
+
+
+
+
+left(90)
+forward (50)
+
+
+
+right(90)
+
+ 
+forward (75)
+
+
+right(90)
+forward (50)
+
+
+
+right(90)
+forward (50)
+
+
+right(90)
+forward (50)
+
+
+
+
+
+right(90)
+forward (100)
+
+
+forward(50)
+
+
+right(90)
+forward (50)
+
+right(90)
+forward (50)
+
+right(90)
+forward (50)
+
+right(90)
+forward (75)
+
+
+
+forward(50)
+
+
+right(90)
+forward (50)
+
+
+
+right(90)
+forward (50)
+
+
+
+right(90)
+forward (50)
+
+forward(250)
+
+
+right(90)
+
+
+forward(200)
+
+
+right(90)
+
+
+forward(400)
+
+
+
+right(90)
+forward(600)
+
+
+
+right(90)
+
+forward(400)
+
+
+right(90)
+
+forward(200)
+
+right(180)
+
+forward(200)
+
+left(90)
+forward(500)
+
+left(90)
+forward(100)
+
+
+left(90)
+forward(100)
+
+
+
+right (90)
+forward(100)
+
+
+
+right (90)
+forward(100)
+
+
+
+left(90)
+forward(100)
+
+left(90)
+forward(100)
+
+
+
+
+right (90)
+forward(100)
+
+
+
+right (90)
+forward(100)
+
+
+
+left(90)
+
+
+
+forward(100)
+
+left(90)
+forward(100)
+
+
+
+right (90)
+forward(100)
+
+
+
+right (90)
+forward(100)
+
+
+
+left(90)
+
+
+
+forward(100)
+
+left(90)
+forward(100)
+
+forward(450)
+
+left(90)
+forward(800)
+
+
+left(90)
+forward(550)
+
+left(90)
+forward(100)
+
+right(120)
+
+forward(100)
+
+right(120)
+
+
+forward (100)
+right(30)
+forward(550)
+
+right(90)
+
+forward(800)
+right(90)
+
+forward(550)
+
+
+right(90)
+forward(100)
+right(90)
+forward(100)
+
+
+
+left(90)
+
+forward(100)
+
+left(90)
+
+forward(200)
+
+right(90)
+
+forward(150)
+right(90)
+
+forward(75)
+right(90)
+
+forward(150)
+
+
+left(90)
+
+forward(125)
+
+
+right(90)
+
+forward(100)
+
+
+right(90)
+
+forward(100)
+
+left(30)
+
+forward(100)
+
+
+left(120)
+
+forward(100)
+
+shape("turtle")
+penup()
+goto(-180,290)
+
+
+
+color("green")
+write("GOA",font=("normal", 30, "normal"))
+
+
+exitonclick()
