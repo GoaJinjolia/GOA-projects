ba2cfa35c7ace73ced69e97095716027ce9cf68b
GoaJinjolia <giojinjo009@gmail.com>
2024-05-27 19:49:53 +0400

    commit

---
+++ day 6/lesson6.py
@@ -0,0 +1,41 @@
+#davaleba 1
+name = input("enter your name")
+surname = input("enter your surname")
+print(name+surname)
+
+
+#davaleba 2
+# num1=5
+# num2 = int(input("enter a number: "))
+# print(num1 + num2)
+
+
+# davaleba 3
+
+# num1=(5)
+# num2=(3)
+# num3=(4)
+# print((num1+num2+num3)/3)
+
+
+
+
+#davaleba 4
+# string=input("enter text here: ")
+# num1=int(input("enter a number here: " ))
+
+# print(string*5)
+# print(num1*5)
+
+
+#davaleba 5
+# num1=int(input("Enter your Number Here: "))
+# num2=int(input("Enter your Number Here: "))
+
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+
+
+
+
