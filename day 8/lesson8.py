545181ba6a1a5851ae4e256dfc300b1d936d3323
GoaJinjolia <giojinjo009@gmail.com>
2024-05-28 19:13:38 +0400

    commit

---
+++ day 8/lesson8.py
@@ -0,0 +1,56 @@
+#davaleba N1
+#>
+print(5 > 2)
+print(10 > 9)
+print(20 > 10)
+print(24 > 11)
+print(15 > 11)
+
+#<
+print(2 < 6)
+print(7 < 8)
+print(99 < 100)
+print(100 < 125)
+print(20 < 21)
+
+#==
+print(27 == 27)
+print(21 == 21)
+print(24 == 24)
+print(26 == 26)
+print(25 == 25)
+
+#>=
+print(6 >= 3)
+print(10 >= 5)
+print(9 >= 7)
+print(4 >= 3)
+print(11 >= 6)
+
+#<=
+print(9 <= 11)
+print(1 <= 14)
+print(5 <= 17)
+print(2 <= 7)
+print(7 <= 10)
+
+#!=
+print(7 != 5)
+print(7 != 5)
+print(7 != 5)
+print(7 != 5)
+print(7 != 5)
+
+#Davaleba N2
+
+#or
+# False or True - True
+# True or False - True
+# True or True - True
+# False or False - False
+
+
+#True and Fales - False
+#False and True - False
+#True and True - True
+#False and False - False
