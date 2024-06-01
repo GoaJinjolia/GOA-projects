02e90c438d96f8f12f9264240adf397ede69a30f
GoaJinjolia <giojinjo009@gmail.com>
2024-06-01 19:14:48 +0400

    commit

---
+++ day 4/lesson4.py
@@ -0,0 +1,28 @@
+#davaleba 1
+# pasi=5
+# raodenoba=10
+# print(pasi*raodenoba)
+
+#davaleba 2
+# myage=14
+# momage=34
+# print(myage+10)
+# print(momage+10)
+# print(momage-myage)
+# print(momage+myage)
+# print(momage*myage)
+# print(momage/myage)
+
+#davaleba 3
+# num1=10
+# num2=5
+# print(10-5)
+# print(10+5)
+# print(10*5)
+# print(10/5)
+
+#davaleba 4
+name=("giorgi")
+age=14
+print(name)
+print(age)
