fc53bd0eeb0d59525f10f162ef94aa4340107036
GoaJinjolia <giojinjo009@gmail.com>
2024-07-24 15:28:29 +0400

    commit

---
+++ day 16/lesson16.py
@@ -0,0 +1,59 @@
+# listn = ["gio","mari","elene"]
+# print(listn[0])
+# print(listn[1])
+# print(listn[2])
+  
+
+
+# listn = [1,2,3,4,5,6,7,8,9,10]
+# print(listn[0])
+# print(listn[-1])
+
+
+
+
+# listen = [10,11,12,13,14,15,16,17,18,19,20]
+# for i in range(len(listen)):
+#     if i % 2 ==0:
+#         listen[i] = 1
+# print(listen)
+
+
+
+# info_list = []
+# num1 = input(enter your name here: )
+# num2 = input(enter your surname here: )
+# num3 = input(enter your age here: )
+# num4 = input(enter your adres here: )
+# num5 = input(enter your gmail here: )
+# info_list.append(num1)
+# info_list.append(num2)
+# info_list.append(num3)
+# info_list.append(num4)
+# info_list.append(num5)
+
+
+
+
+
+# surname = "Jinjolia"
+# print(listn[0])
+# print(listn[1])
+# print(listn[2])
+# print(listn[3])
+# print(listn[4])
+# print(listn[5])
+# print(listn[6])
+# print(listn[7])
+
+
+
+
+
+
+
+
+# listn = []
+# for i in range(101):
+#     listn.append(i)
+#     print(listn)
---
+++ day 20/lesson20.py
@@ -0,0 +1,21 @@
+# def hello():
+#     print("GOA BEST")
+# hello()
+
+
+# def sumnumbers(a,b):
+#     print(a+b)
+# sumnumbers(5,2)
+
+# def to_strings(word):
+#     print(word)
+# to_strings(5)
+
+# def print_type(x):
+#     print(type(x))
+# print_type("ssadw")
+ 
+
+def to_int(number):
+    print(int(number))
+to_int("5")
